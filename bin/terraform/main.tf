53fe74a8a192c7d8739fcf4e34836846654f70aa
Duncan Fedde <duncan@fedde.us>
2018-01-29 19:03:52 -0700

    wip

--- bin/terraform/main.tf
+++ bin/terraform/main.tf
@@ -23,23 +23,24 @@ resource "google_project_iam_member" "project" {
   member  = "user:${element(var.students, count.index)}"
 }
 
-resource "google_project_iam_member" "terrafrom" {
-  count = "${var.student_count}"
-  project = "${google_project.class_projects.*.number[count.index]}"
-  role    = "roles/editor"
-  member      = "serviceAccount:${google_service_account.terraform_user.*.email[count.index]}"
-}
+/* resource "google_project_iam_member" "terrafrom" { */
+/*   count = "${var.student_count}" */
+/*   project = "${google_project.class_projects.*.number[count.index]}" */
+/*   role    = "roles/editor" */
+/*   member      = "serviceAccount:${google_service_account.terraform_user.*.email[count.index]}" */
+/* } */
 
-resource "google_service_account" "terraform_user" {
-  count = "${var.student_count}"
-  account_id   = "terraform-user"
-  display_name = "Terrafrom User"
-  project = "${google_project.class_projects.*.number[count.index]}"
-}
+/* resource "google_service_account" "terraform_user" { */
+/*   count = "${var.student_count}" */
+/*   account_id   = "terraform-user" */
+/*   display_name = "Terrafrom User" */
+/*   project = "${google_project.class_projects.*.number[count.index]}" */
+/* } */
 
 resource "google_storage_bucket" "asset_store" {                                           
   count = "${var.student_count}"
   name="${google_project.class_projects.*.project_id[count.index]}-assets"
+  project="${google_project.class_projects.*.number[count.index]}"
   storage_class = "MULTI_REGIONAL"
 }
 
